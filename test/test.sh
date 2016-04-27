ded85e8206ab93962b09f3dbb1e2239f976b7812
Tamas Kalman <cruizer@cruisers.hu>
2016-04-27 08:09:47 +0200

    Refactored test to work with sourced functions

--- test/test.sh
+++ test/test.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
+. ../passmgr.sh testsource
 testCheckParnumSuccesRetcode()
 {
   local result
-  result=`../passmgr.sh --shunit2 check_parnum 3 3`
+  result=$(check_parnum 3 3)
   assertTrue "check_parnum returned with non-zero code" $?
 }
 testCheckParnumSuccessOutput()
 {
   local result
-  result=`../passmgr.sh --shunit2 check_parnum 3 3`
+  result=$(check_parnum 3 3)
   assertNull "check_parnum returned output" "$result"
 }
 testCheckParnumFailureRetcode()
 {
   local result
-  result=`../passmgr.sh --shunit2 check_parnum 3 4`
+  result=$(check_parnum 3 4)
   assertFalse "check_parnum returned with zero code" $?
 }
 testCheckParnumFailureOutput()
 {
   local result expected
-  result=`../passmgr.sh --shunit2 check_parnum 3 4 | head -n1`
+  result=$(check_parnum 3 4 | head -n1)
   expected="Illegal number of parameters."
   assertEquals "check_parnum returned invalid output" \
     "Illegal number of parameters." "$result"
